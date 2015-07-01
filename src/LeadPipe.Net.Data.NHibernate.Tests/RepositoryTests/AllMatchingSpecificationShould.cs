9605490bc4b37ecd24767dcc968d289b20b74d2d
Greg Major <greg.b.major@gmail.com>
2015-07-01 14:37:57 -0500

    Added some more specification unit tests.

--- src/LeadPipe.Net.Data.NHibernate.Tests/RepositoryTests/AllMatchingSpecificationShould.cs
+++ src/LeadPipe.Net.Data.NHibernate.Tests/RepositoryTests/AllMatchingSpecificationShould.cs
@@ -54,6 +54,114 @@ namespace LeadPipe.Net.Data.NHibernate.Tests.RepositoryTests
 			}
 		}
 
+        /// <summary>
+        /// Tests that AllMatchingSpecification returns all matching objects given an AndSpecification.
+        /// </summary>
+        [Test]
+        public void ReturnAllMatchingObjectsGivenAndSpecification()
+        {
+            // Arrange
+            Bootstrapper.Start();
+
+            var repository = ObjectFactory.GetInstance<Repository<TestModel>>();
+            var unitOfWorkFactory = ObjectFactory.GetInstance<IUnitOfWorkFactory>();
+            var unitOfWork = unitOfWorkFactory.CreateUnitOfWork();
+
+            var testModel01 = new TestModel("ABCDEF");
+            var testModel02 = new TestModel("HIJKLM");
+            var testModel03 = new TestModel("ABCXYZ");
+
+            // Act
+            using (unitOfWork.Start())
+            {
+                repository.Create(testModel01);
+                repository.Create(testModel02);
+                repository.Create(testModel03);
+
+                unitOfWork.Commit();
+            }
+
+            // Assert
+            using (unitOfWork.Start())
+            {
+                var foundModel = repository.Find.AllMatchingSpecification(TestModelSpecifications.TestPropertyStartsWithABCAndEndsWithXYZ()).ToList();
+
+                Assert.That(foundModel.Count.Equals(1));
+            }
+        }
+
+        /// <summary>
+        /// Tests that AllMatchingSpecification returns all matching objects given an OrSpecification.
+        /// </summary>
+        [Test]
+        public void ReturnAllMatchingObjectsGivenOrSpecification()
+        {
+            // Arrange
+            Bootstrapper.Start();
+
+            var repository = ObjectFactory.GetInstance<Repository<TestModel>>();
+            var unitOfWorkFactory = ObjectFactory.GetInstance<IUnitOfWorkFactory>();
+            var unitOfWork = unitOfWorkFactory.CreateUnitOfWork();
+
+            var testModel01 = new TestModel("ABCDEF");
+            var testModel02 = new TestModel("HIJKLM");
+            var testModel03 = new TestModel("ABCXYZ");
+
+            // Act
+            using (unitOfWork.Start())
+            {
+                repository.Create(testModel01);
+                repository.Create(testModel02);
+                repository.Create(testModel03);
+
+                unitOfWork.Commit();
+            }
+
+            // Assert
+            using (unitOfWork.Start())
+            {
+                var foundModel = repository.Find.AllMatchingSpecification(TestModelSpecifications.TestPropertyStartsWithABCOrEndsWithXYZ()).ToList();
+
+                Assert.That(foundModel.Count.Equals(2));
+            }
+        }
+
+        /// <summary>
+        /// Tests that AllMatchingSpecification returns all matching objects given a NotSpecification.
+        /// </summary>
+        [Test]
+        public void ReturnAllMatchingObjectsGivenNotSpecification()
+        {
+            // Arrange
+            Bootstrapper.Start();
+
+            var repository = ObjectFactory.GetInstance<Repository<TestModel>>();
+            var unitOfWorkFactory = ObjectFactory.GetInstance<IUnitOfWorkFactory>();
+            var unitOfWork = unitOfWorkFactory.CreateUnitOfWork();
+
+            var testModel01 = new TestModel("ABCDEF");
+            var testModel02 = new TestModel("HIJKLM");
+            var testModel03 = new TestModel("ABCXYZ");
+
+            // Act
+            using (unitOfWork.Start())
+            {
+                repository.Create(testModel01);
+                repository.Create(testModel02);
+                repository.Create(testModel03);
+
+                unitOfWork.Commit();
+            }
+
+            // Assert
+            using (unitOfWork.Start())
+            {
+                var foundModel = repository.Find.AllMatchingSpecification(TestModelSpecifications.NotTestPropertyStartsWithABCOrEndsWithXYZ()).ToList();
+
+                Assert.That(foundModel.Count.Equals(1));
+            }
+        }
+
 		/// <summary>
 		/// Tests that AllMatchingSpecification does not return objects that do not match.
 		/// </summary>
--- src/LeadPipe.Net.Data.NHibernate.Tests/TestModelSpecifications.cs
+++ src/LeadPipe.Net.Data.NHibernate.Tests/TestModelSpecifications.cs
@@ -26,6 +26,48 @@ namespace LeadPipe.Net.Data.NHibernate.Tests
 			return new AdHocSpecification<TestModel>(x => x.TestProperty.StartsWith("ABC"));
 		}
 
+        /// <summary>
+        /// The test property ends with xyz.
+        /// </summary>
+        /// <returns>
+        /// Test Models that end with XYZ.
+        /// </returns>
+	    public static ISpecification<TestModel> TestPropertyEndsWithXYZ()
+	    {
+	        return new AdHocSpecification<TestModel>(x => x.TestProperty.EndsWith("XYZ"));
+	    }
+
+        /// <summary>
+        /// The test property starts with abc and ends with xyz.
+        /// </summary>
+        /// <returns>
+        /// Test Models that start with ABC and end with XYZ.
+        /// </returns>
+	    public static ISpecification<TestModel> TestPropertyStartsWithABCAndEndsWithXYZ()
+	    {
+	        return new AndSpecification<TestModel>(TestPropertyStartsWithABC(), TestPropertyEndsWithXYZ());
+	    }
+
+        /// <summary>
+        /// The test property starts with abc or ends with xyz.
+        /// </summary>
+        /// <returns>
+        /// Test Models that start with ABC or end with XYZ.
+        /// </returns>
+        public static ISpecification<TestModel> TestPropertyStartsWithABCOrEndsWithXYZ()
+        {
+            return new OrSpecification<TestModel>(TestPropertyStartsWithABC(), TestPropertyEndsWithXYZ());
+        }
+
+        /// <summary>
+        /// The test property does NOT start with abc or ends with xyz.
+        /// </summary>
+        /// <returns></returns>
+        public static ISpecification<TestModel> NotTestPropertyStartsWithABCOrEndsWithXYZ()
+        {
+            return new NotSpecification<TestModel>(TestPropertyStartsWithABC());
+        }
+
 		#endregion
 	}
 }
