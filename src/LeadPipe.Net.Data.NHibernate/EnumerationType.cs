38fb817cb80d03e022a53ad48e29cdbea2999db4
Greg Major <greg.b.major@gmail.com>
2016-05-03 14:03:40 -0500

    Added the WellKnownInstanceType and updated the EnumerationType

--- src/LeadPipe.Net.Data.NHibernate/EnumerationType.cs
+++ src/LeadPipe.Net.Data.NHibernate/EnumerationType.cs
@@ -1,78 +1,25 @@
-﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="EnumerationType.cs" company="Lead Pipe Software">
-//     Copyright (c) Lead Pipe Software All rights reserved.
-// </copyright>
-// --------------------------------------------------------------------------------------------------------------------
-
-using System;
-using System.Data;
-using NHibernate.Dialect;
-using NHibernate.SqlTypes;
-using NHibernate.Type;
+﻿using System;
+using LeadPipe.Net.Extensions;
 
 namespace LeadPipe.Net.Data.NHibernate
 {
-    /// <summary>
-    /// A primitive type declaration to support NHibernate mapping for Enumerations.
-    /// </summary>
-    /// <typeparam name="T">The Enumeration type.</typeparam>
-    public class EnumerationType<T> : PrimitiveType where T : Enumeration<T>
+    [Serializable]
+    public class EnumerationType<T> : WellKnownInstanceType<Enumeration<T>> where T : Enumeration<T>
     {
-        public EnumerationType()
-            : base(new SqlType(DbType.Int32))
+        public EnumerationType() : base(Enumeration<T>.GetAll(), FindPredicate, GetId)
         {
         }
 
-        public override object Get(IDataReader rs, int index)
+        private static bool FindPredicate(Enumeration<T> instance, int id)
         {
-            var o = rs[index];
-            var value = Convert.ToInt32(o);
-            return Enumeration<T>.FromInt32(value);
+            if (instance.IsNull()) return false;
+
+            return instance.Value == id;
         }
 
-        public override object Get(IDataReader rs, string name)
+        private static int GetId(Enumeration<T> instance)
         {
-            var ordinal = rs.GetOrdinal(name);
-            return Get(rs, ordinal);
-        }
-
-        public override Type ReturnedClass
-        {
-            get { return typeof(T); }
-        }
-
-        public override object FromStringValue(string xml)
-        {
-            return int.Parse(xml);
-        }
-
-        public override string Name
-        {
-            get { return "enumeration"; }
-        }
-
-        public override void Set(IDbCommand cmd, object value, int index)
-        {
-            var parameter = (IDataParameter)cmd.Parameters[index];
-
-            var val = (Enumeration<T>)value;
-
-            parameter.Value = val.Value;
-        }
-
-        public override string ObjectToSQLString(object value, Dialect dialect)
-        {
-            return value.ToString();
-        }
-
-        public override Type PrimitiveClass
-        {
-            get { return typeof(int); }
-        }
-
-        public override object DefaultValue
-        {
-            get { return 0; }
+            return instance.Value;
         }
     }
 }
---
+++ src/LeadPipe.Net.Data.NHibernate/WellKnownInstanceType.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using NHibernate.SqlTypes;
+using NHibernate.UserTypes;
+
+namespace LeadPipe.Net.Data.NHibernate
+{
+    [Serializable]
+    public abstract class WellKnownInstanceType<T, TId> : IUserType where T : class
+    {
+        //private static readonly SqlType[] ReturnSqlTypes = { SqlTypeFactory.Int32 };
+        private Func<T, TId, bool> findPredicate;
+        private Func<T, TId> idGetter;
+        private IEnumerable<T> repository;
+
+        /// <summary>
+        /// Base constructor
+        /// </summary>
+        /// <param name="repository">The collection that represent a in-memory repository.</param>
+        /// <param name="findPredicate">The predicate an instance by the persisted value.</param>
+        /// <param name="idGetter">The getter of the persisted value.</param>
+        protected WellKnownInstanceType(IEnumerable<T> repository, Func<T, TId, bool> findPredicate, Func<T, TId> idGetter)
+        {
+            this.repository = repository;
+            this.findPredicate = findPredicate;
+            this.idGetter = idGetter;
+        }
+
+        public bool IsMutable
+        {
+            get { return false; }
+        }
+
+        //public SqlType[] SqlTypes
+        //{
+        //    get { return ReturnSqlTypes; }
+        //}
+        public Type ReturnedType
+        {
+            get { return typeof(T); }
+        }
+
+        /// <summary>
+        /// The SQL types for the columns mapped by this type.
+        /// </summary>
+        public abstract SqlType[] SqlTypes { get; }
+
+        public object Assemble(object cached, object owner)
+        {
+            return cached;
+        }
+
+        public object DeepCopy(object value)
+        {
+            return value;
+        }
+
+        public object Disassemble(object value)
+        {
+            return value;
+        }
+
+        public new bool Equals(object x, object y)
+        {
+            if (ReferenceEquals(x, y))
+            {
+                return true;
+            }
+            if (ReferenceEquals(null, x) || ReferenceEquals(null, y))
+            {
+                return false;
+            }
+            return x.Equals(y);
+        }
+
+        public int GetHashCode(object x)
+        {
+            return (x == null) ? 0 : x.GetHashCode();
+        }
+
+        public object NullSafeGet(IDataReader rs, string[] names, object owner)
+        {
+            int index0 = rs.GetOrdinal(names[0]);
+            if (rs.IsDBNull(index0))
+            {
+                return null;
+            }
+            var value = (TId)rs.GetValue(index0);
+            return repository.FirstOrDefault(x => findPredicate(x, value));
+        }
+
+        public void NullSafeSet(IDbCommand cmd, object value, int index)
+        {
+            if (value == null)
+            {
+                ((IDbDataParameter)cmd.Parameters[index]).Value = DBNull.Value;
+            }
+            else
+            {
+                ((IDbDataParameter)cmd.Parameters[index]).Value = idGetter((T)value);
+            }
+        }
+
+        public object Replace(object original, object target, object owner)
+        {
+            return original;
+        }
+    }
+
+    /// <summary>
+    /// A <see cref="IUserType"/> to manage relationships with a well know entities.
+    /// </summary>
+    /// <typeparam name="T">The type of the wellknow entity</typeparam>
+    /// <remarks>
+    /// <typeparamref name="T"/> is the type to use in the entity owning the relation, the type in the persistence is <see cref="int"/>.
+    /// </remarks>
+    [Serializable]
+    public abstract class WellKnownInstanceType<T> : WellKnownInstanceType<T, int> where T : class
+    {
+        private static readonly SqlType[] sqlTypes = new[] { SqlTypeFactory.Int32 };
+
+        /// <summary>
+        /// Base constructor
+        /// </summary>
+        /// <param name="repository">The collection that represent a in-memory repository.</param>
+        /// <param name="findPredicate">The predicate an instance by the persisted value.</param>
+        /// <param name="idGetter">The getter of the persisted value.</param>
+        protected WellKnownInstanceType(IEnumerable<T> repository, Func<T, int, bool> findPredicate, Func<T, int> idGetter) : base(repository, findPredicate, idGetter)
+        {
+        }
+
+        public override SqlType[] SqlTypes
+        {
+            get { return sqlTypes; }
+        }
+    }
+}
